09397191b4103ee1bf074bdb532a850d015fda30
Andres Duarte <andres_fduarte@soy.sena.edu.co>
2025-03-06 21:39:18 -0500

    Agregada funcionalidad de Evidencia con CRUD

--- PTC/src/Main.kt
+++ PTC/src/Main.kt
@@ -1,9 +1,12 @@
 import Administrador.Clientes.CRUD
 import Administrador.Contrato
+import Detective.EvidenciaCRUD
 
 fun main() {
 
-    val contrato = Contrato()
+    //val contrato = Contrato()
+    val evidenciaCRUD = EvidenciaCRUD()
+    evidenciaCRUD.iniciar()
    
 
 
